1642ac805f96dba6670179c99770dcb286464818
= <=>
2023-06-28 11:35:53 +0800

    minor updates

--- js/bundle.js
+++ js/bundle.js
@@ -1,25 +1,25 @@
 // mobile menu toggle
-// const primaryNav = document.querySelector('.primary-nav__list');
-// const menuToggle = document.querySelector('.menu-toggle');
+const primaryNav = document.querySelector('.primary-nav__list');
+const menuToggle = document.querySelector('.menu-toggle');
 
-// menuToggle.addEventListener('click', () => {
-//     const isOpened = menuToggle.getAttribute('aria-expanded') ==="true";
-//     if (isOpened ? closeMenu() : openMenu());
-// });
+menuToggle.addEventListener('click', () => {
+    const isOpened = menuToggle.getAttribute('aria-expanded') ==="true";
+    if (isOpened ? closeMenu() : openMenu());
+});
 
-// function openMenu() {
-//     menuToggle.setAttribute('aria-expanded', "true");
-//     primaryNav.setAttribute('data-state', "opened")
-// }
+function openMenu() {
+    menuToggle.setAttribute('aria-expanded', "true");
+    primaryNav.setAttribute('data-state', "opened")
+}
 
-// function closeMenu() {
-//     menuToggle.setAttribute('aria-expanded', "false");
-//     primaryNav.setAttribute('data-state', "closing")
+function closeMenu() {
+    menuToggle.setAttribute('aria-expanded', "false");
+    primaryNav.setAttribute('data-state', "closing")
 
-//     primaryNav.addEventListener('animationend', () => {
-//         primaryNav.setAttribute('data-state', "closed")
-//     }, {once: true});
-// }
+    primaryNav.addEventListener('animationend', () => {
+        primaryNav.setAttribute('data-state', "closed")
+    }, {once: true});
+}
 
 // image slider
 const slider = document.querySelector('.slider');
